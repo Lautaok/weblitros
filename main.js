8e47bcc74d8ef254a66458dcef3c8158d94e4095
lautaro vera <lautarovera1709@gmail.com>
2024-08-08 22:49:47 -0300

    cambios

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ document.getElementById('calculatorForm').addEventListener('submit', function(e)
         const chlorineAmount = (poolVolume * 1) / 20000; // 1 litro de cloro por 20,000 litros
         const algaecideAmount = (poolVolume * 80) / 20000; // 80 ml de alguicida por 20,000 litros
         const clarifierAmount = (poolVolume * 200) / 20000; // 200 ml de clarificante por 20,000 litros
+        const cloroPolvoAmount = (poolVolume * 4) / 10000; // 4 cucharadas de cloro en polvo cada 10.000 litros
         
         document.getElementById('chlorineResult').innerText = `Cloro Líquido: ${chlorineAmount.toFixed(2)} litros`;
         document.getElementById('algaecideResult').innerText = `Alguicida: ${algaecideAmount.toFixed(2)} ml`;
         document.getElementById('clarifierResult').innerText = `Clarificante: ${clarifierAmount.toFixed(2)} ml`;
+        document.getElementById('cloroPolvoResult').innerText = `Cloro Polvo: ${cloroPolvoAmount.toFixed(0)} cucharada`;
     } else {
         document.getElementById('results').innerText = 'Por favor, introduce un volumen válido.';
     }
