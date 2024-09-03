3010fffd7135e2cbb9ab67f0611a4843e3a30b84
Lautaok <167364169+Lautaok@users.noreply.github.com>
2024-09-03 15:50:35 -0300

    Add files via upload
    cambios general

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ document.getElementById('calculatorForm').addEventListener('submit', function(e)
         document.getElementById('results').innerText = 'Por favor, introduce un volumen válido.';
     }
 });
+
+//Play an animation back on second click
+
+let iconMenu = document.querySelector('.bodymovinanim');
+
+    let animationMenu = bodymovin.loadAnimation({
+            container: iconMenu,
+            renderer: 'svg',
+            loop: false,
+            autoplay: false,
+            path: "https://raw.githubusercontent.com/thesvbd/Lottie-examples/master/assets/animations/calendar.json"
+    });
+
+    var directionMenu = 1;
+      iconMenu.addEventListener('mouseenter', (e) => {
+      animationMenu.setDirection(directionMenu);
+      animationMenu.play();
+    });
+
+      iconMenu.addEventListener('mouseleave', (e) => {
+      animationMenu.setDirection(-directionMenu);
+      animationMenu.play();
+    });
